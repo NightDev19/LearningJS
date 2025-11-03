6ecfdf2fe2251e71a1f565372e650bdf371f4dc1
NightDev19 <sherwintajan143@gmail.com>
2025-11-03 15:38:19 +0800

    Update Logical operator in Conditional Statement

--- Conditional_Statement/logical_operator/index.js
+++ Conditional_Statement/logical_operator/index.js
@@ -1,15 +1,19 @@
-//Conditional Logic with &&, ||, and ??
+// Conditional Logic with &&, ||, ??, and !
 
 let x = 15; // sample value
 
-// Runs only if x > 10
-x > 10 && console.log("x is large");     
+// Executes only if x > 10
+x > 10 && console.log("x is large");
 
-// Runs only if x < 5 is false
-x < 5 || console.log("x is not small");  
+// Executes only if x < 5 is false
+x < 5 || console.log("x is not small");
 
 // Uses "Default" if userInput is null or undefined
 let userInput = null;
-let value = userInput ?? "Default";      
+let value = userInput ?? "Default";
 
 console.log("value:", value);
+
+x = 0; // lets change the value of x to 0
+// Executes only if x is falsy (0, null, undefined, false, "")
+!x && console.log("x is false");
